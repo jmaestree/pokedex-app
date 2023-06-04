748db8b100ab331e8666cea81f18244dba096ce4
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-04 13:27:10 +0200

    chore: :wastebasket: remove dead code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App layout', () => {
+  test('renders main content', () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/My app/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
