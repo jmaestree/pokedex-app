13f95c33ec35ad4179b0fe31d5871f41872122f4
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-05 12:13:06 +0200

    refactor: :recycle: add BASE URL to API urls

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,5 +1,9 @@
+// NOTE: This can be an ENV variable,
+// I put it here as a constant in order to avoid error configuring ENV variables
+const BASE_URL = 'http://localhost:3001';
+
 export const urls = {
-  getAll: '/characters'
+  getAll: () => `${BASE_URL}/characters`
 } as const;
 
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then((res) => res.json());
