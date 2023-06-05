2133bf22c0d5b295e0c5a1ee5c2c5dff6252bbfa
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-05 11:44:39 +0200

    feat: :sparkles: consume DataTable on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import DataTable from './components/data-table/data-table';
 
 function App() {
   return (
     <main>
       <Navbar />
-      <div className="flex px-4 pb-10 flex-1 gap-x-3">My app - To be created</div>
+      <div className="flex px-4 pb-10 flex-1 gap-x-3">
+        <DataTable />
+      </div>
     </main>
   );
 }
