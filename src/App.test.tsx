59067e94558be527b52adb28edbfaca5deaac5a8
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-05 18:45:40 +0200

    feat: :sparkles: consume API on main page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,96 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import * as useSWR from 'swr';
+
+jest.mock('swr', () => jest.fn());
+jest.mock('./components/loader', () => () => 'LoaderComponent');
+jest.mock('./components/navbar/navbar', () => () => 'NavbarComponent');
+jest.mock('./components/data-table/data-table', () => () => 'DataTableComponent');
 
 describe('App layout', () => {
-  test('renders the navbar', () => {
-    render(<App />);
+  describe('When there is an error', () => {
+    const errorSWR = {
+      isLoading: false,
+      error: {
+        foo: 'bar'
+      },
+      data: undefined
+    } as any;
 
-    const headingElement = screen.getByRole('heading', { level: 1, name: /Pokédex/i });
-    expect(headingElement).toBeInTheDocument();
+    test('renders the right content', () => {
+      const spySwr = jest.spyOn(useSWR, 'default');
+      spySwr.mockReturnValue(errorSWR);
+
+      render(<App />);
+
+      const text = screen.getByText("There's an error retrieving data from API");
+      expect(text).toBeInTheDocument();
+      expect(spySwr).toHaveBeenCalled();
+    });
   });
 
-  test('renders main content', () => {
-    render(<App />);
+  describe('When there is no data', () => {
+    const emptySWR = {
+      isLoading: false,
+      error: undefined,
+      data: undefined
+    } as any;
 
-    const tableElement = screen.getByRole('table');
-    expect(tableElement).toBeInTheDocument();
+    test('renders the right content', () => {
+      const spySwr = jest.spyOn(useSWR, 'default');
+      spySwr.mockReturnValue(emptySWR);
+
+      render(<App />);
+
+      const text = screen.getByText("There's no data available");
+      expect(text).toBeInTheDocument();
+      expect(spySwr).toHaveBeenCalled();
+    });
+  });
+
+  describe('When IS loading', () => {
+    const loadingSWR = {
+      isLoading: true,
+      error: undefined,
+      data: undefined
+    } as any;
+
+    test('renders the right content', () => {
+      const spySwr = jest.spyOn(useSWR, 'default');
+      spySwr.mockReturnValue(loadingSWR);
+      render(<App />);
+
+      const loaderElement = screen.getByText('LoaderComponent');
+      expect(loaderElement).toBeInTheDocument();
+    });
+  });
+
+  describe('When IS NOT loading and data exists', () => {
+    const successSWR = {
+      isLoading: false,
+      error: undefined,
+      data: {
+        foo: 'bar'
+      }
+    } as any;
+
+    test('renders the navbar', () => {
+      const spySwr = jest.spyOn(useSWR, 'default');
+      spySwr.mockReturnValue(successSWR);
+      render(<App />);
+
+      const navbarElement = screen.getByText('NavbarComponent');
+      expect(navbarElement).toBeInTheDocument();
+    });
+
+    test('renders main content', () => {
+      const spySwr = jest.spyOn(useSWR, 'default');
+      spySwr.mockReturnValue(successSWR);
+      render(<App />);
+
+      const dataTableElement = screen.getByText('DataTableComponent');
+      expect(dataTableElement).toBeInTheDocument();
+    });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/navbar';
 import DataTable from './components/data-table/data-table';
+import useSWR from 'swr';
+import fetcher, { urls } from './api/fetcher';
+import { Pokemon } from './api/types';
+import Loader from './components/loader';
 
 function App() {
+  const { data, error, isLoading } = useSWR<Pokemon[]>(urls.getAll, fetcher);
+
+  if (isLoading) {
+    return (
+      <div className="flex w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p>There's an error retrieving data from API</p>;
+  }
+
+  if (!data) {
+    return <p>There's no data available</p>;
+  }
+
   return (
     <main>
       <Navbar />
       <div className="flex px-4 pb-10 flex-1 gap-x-3">
-        <DataTable />
+        <DataTable
+          data={data}
+          columns={[
+            {
+              key: 'id',
+              title: 'ID'
+            },
+            {
+              key: 'name',
+              title: 'Name'
+            },
+            {
+              key: 'order',
+              title: 'Image'
+            }
+          ]}
+        />
       </div>
     </main>
   );
