4f2b84359b6de2c1ab68904d943ae034bd7fc0a0
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-06 10:32:53 +0200

    feat: :sparkles: add new query

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -3,7 +3,9 @@
 const BASE_URL = 'http://localhost:3001';
 
 export const urls = {
-  getAll: () => `${BASE_URL}/characters`
+  getAll: () => `${BASE_URL}/characters`,
+  getById: (id: string) => `${BASE_URL}/characters/${id}`
 } as const;
 
 export const getAllPokemons = () => fetch(urls.getAll()).then((res) => res.json());
+export const getPokemon = ([_key, id]: string[]) => fetch(urls.getById(id)).then((res) => res.json());
