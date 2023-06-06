209b9b11e37e574da0ad2945ccb6079925b11840
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-06-06 12:28:50 +0200

    feat: :sparkles: add Link to navigate home in navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
     <div className="flex flex-row p-4 mb-3 bg-white border-b shadow justify-between items-center">
-      <h1 className="text-xl font-bold">Pokédex</h1>
+      <h1 className="text-xl font-bold">
+        <Link to="/">Pokédex</Link>
+      </h1>
     </div>
   );
 };
